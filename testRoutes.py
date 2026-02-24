69993d903700b9b75158ecdc7217714b428c448e
Vipulkumar Warthe <126955342+vipulwarthe@users.noreply.github.com>
2026-02-24 16:02:21 +0530

    Delete testRoutes.py

--- testRoutes.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-def test_home():
-    tester = app.test_client()
-    response = tester.get('/')
-
-    assert response.status_code == 200
-    assert b"Welcome to Jenkins Tutorials" in response.data
